a5a34cd3f2bfe092b099f3943175a67f8c9647f1
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-02-25 13:39:47 -0300

    feat: criacao de superusuario ligacao de chaves estrangeiras

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,7 +7,19 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         """Devolve uma representação em string do modelo"""
         return self.text
+    
+class Entry(models.Model):
+    """Algo específico aprendido sobre um assunto"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Devolve uma representação em string do modelo"""
+        return self.text[:50] + '...'
