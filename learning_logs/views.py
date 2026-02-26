51239d5bb70a9fb2678d0a3d5ce02d556c7086c2
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-02-26 16:34:18 -0300

    feat: primeira interação com bd

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
@@ -6,4 +7,11 @@ def index(request):
     return render (request, 'learning_logs/index.html')
 
 def teste(request):
+    """Teste criado para aperfiçoar habilidades"""
     return render (request, 'learning_logs/teste.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
