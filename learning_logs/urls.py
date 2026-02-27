5b4cc266118f3e904e59e30b9a82973a821ad8d4
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-02-27 14:55:50 -0300

    feat: iniciando form para obter dados e upar no bd

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('teste', views.teste, name='teste'),
     path('topics', views.topics, name='topics'),
     path('topics/<topic_id>/', views.topic, name='topic'),
+    path('new_topic', views.new_topic, name='new_topic')
 ]
